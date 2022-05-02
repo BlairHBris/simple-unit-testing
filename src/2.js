b5876661dac4a27e0e2bcbae479a82afa27ccfb2
Blair Bristow <blairhbristow@gmail.com>
2022-05-02 11:45:16 -0400

    Add whoWon test

--- src/2.js
+++ src/2.js
@@ -18,4 +18,6 @@ function whoWon(player1,player2){
   return 'Player 1 wins!';
 }
 
-module.exports = whoWon
+module.exports = {
+  whoWon,
+}
--- tests/all.test.js
+++ tests/all.test.js
@@ -1,4 +1,6 @@
+const { expect } = require("expect")
 const { checkFive } = require("../src/1")
+const { whoWon } = require("../src/2")
 
 describe('checkFive function tests', () => {
     it('confirms a given number is equal to 5', () => {
@@ -15,3 +17,21 @@ describe('checkFive function tests', () => {
     })
 })
 
+describe('whoWon function tests', () => {
+    it('returns tie if the players are equal', () => {
+        const result = whoWon('rock','rock')
+        expect(result).toBe('TIE!')
+    })
+    it('returns the winner in a rock vs scissors scenario', () => {
+        const result = whoWon('scissors','rock')
+        expect(result).toBe('Player 2 wins!')
+    })
+    it('returns the winner in a rock vs paper scenario', () => {
+        const result = whoWon('rock','paper')
+        expect(result).toBe('Player 2 wins!')
+    })
+    it('returns the winner in a scissors vs paper scenario', () => {
+        const result = whoWon('scissors','paper')
+        expect(result).toBe('Player 1 wins!')
+    })
+})
